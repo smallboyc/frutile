df5609cd09233f6d0a352697e1cb497aaceff85b
smallboyc <maxencedup44@gmail.com>
2025-01-11 21:42:38 +0100

    :sparkles: setup main font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import "./styles/global.css";
 function App() {
   return (
     <div>
-      <h1>Hello fr'utile !</h1>
+      <h1 className="leckerli-one-regular">Hello fr'utile !</h1>
     </div>
   );
 }
