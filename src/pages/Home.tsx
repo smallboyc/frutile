d7a54e99e935e6c92b9f02734a9ad16c936450ee
smallboyc <maxencedup44@gmail.com>
2025-01-12 01:46:05 +0100

    :alien: get data from api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
--- src/pages/Yard.tsx
+++ src/pages/Yard.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import fetchFruits from "../api/fetchData";
+import { Fruit } from "../types/Fruit";
 
 const Yard = () => {
+  const [fruits, setFruits] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getFruits = async () => {
+      try {
+        const data = await fetchFruits();
+        setFruits(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    getFruits();
+  }, []);
+
   return (
     <>
       <h1 className="leckerli-one-regular">Hello Yard !</h1>
       <NavLink to="/">Home</NavLink>
+      {loading ? (
+        <p>Loading fruits...</p>
+      ) : (
+        fruits.map((fruit: Fruit) => <p key={fruit.id}>{fruit.name}</p>)
+      )}
     </>
   );
 };
